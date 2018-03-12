c225f6ba6241cd9d5969bf63d55bca68ab2bdb04
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-12 23:10:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word
+  attr_accessor :word, :word_array
 
   def initialize(word)
     @word = word
   end
 
   def match(word_array)
-    
+    @
+
   end
 
 end
